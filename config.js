c18ca832e7671d696c683ba4f9428f7b025abca2
Vishaljeph22 <84806302+Vishaljeph22@users.noreply.github.com>
2022-11-15 06:36:32 +0530

    come done

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Calling you",
+    introTitle: 'Dear, Sonia',
+    introDesc: `We had amazing time on my last trip to jaipur cheers to that. Also, if you want have more have fun click on "more fun"`,
+    btnIntro: 'More Fun',
+    title: 'haha I got you now this time you have to come',
+    desc: 'will you came to vadodara to meet your bestest friend?',
+    btnYes: 'Yess 1000 times yess',
+    btnNo: 'Big fat no',
+    question: 'I knew it you will choose yes. now please confirm your choice😘',
+    btnReply: 'Confirm',
+    reply: 'Also as you know i cant wait for long please mention when are you coming and all the other details',
+    mess: 'Also as you know i cant wait for long. So, please mention when are you coming and all the other details',
+    messDesc: 'click on the below botton to submit details',
+    btnAccept: 'Details',
+    messLink: 'https://mail.google.com/mail/?view=cm&fs=1&to=kinjalchristian810@gmail.com&su=RegardingTripToVadodara&body=DearLove&bcc=kinjalchristian810@gmail.com' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
+
+
